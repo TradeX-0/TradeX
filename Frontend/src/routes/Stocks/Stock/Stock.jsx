69ca2f96f6374195112cea5a42b3c5c4263854fc
laplace0-0 <swayammhaskar0@gmail.com>
2024-08-13 06:56:39 -0700

    stock name

--- Frontend/src/routes/Stocks/Stock/Stock.jsx
+++ Frontend/src/routes/Stocks/Stock/Stock.jsx
@@ -1,13 +1,37 @@
 import './Stock.css'
 import Chart from '../../../components/graph/Charts'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 function Stock() {
+  const [data, setData] = useState(null)
+
+  const { stock } = useParams()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/stock-price/${ stock }`);
+        const result = await response.json();
+
+        setData(result.meta.shortName);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    
+    fetchData();
+  });
   
   return (
     <>
+      <div className='data'>
+        <p>{data}</p>
+      </div>
       <Chart />
     </>
   )
 }
+
 
 export default Stock
