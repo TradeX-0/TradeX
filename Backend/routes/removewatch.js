46328ee2b9c793b126f5a92ad0be1ed8ff05779c
swayam <swayammhasakr0@gmail.com>
2024-09-21 15:42:23 +0530

    ook

--- Backend/routes/removewatch.js
+++ Backend/routes/removewatch.js
@@ -24,11 +24,7 @@ router.post('/remove', async (req, res) => {
         }
 
         // Check if there are stocks
-        if (data.length === 0) {
-            return res.send([]); // Send an empty array if no stocks found
-        } else {
-            return res.send(data); // Send the fetched datazz
-        }
+            return res.send([]); // Send the fetched datazz
     } catch (error) {
         console.error('Error during fetching user stocks:', error); // Log the error for debugging
         return res.status(500).json({ error: 'Internal server error' });
